f32d638993f86b01f586f441e77b398d235b3e94
datudou <wangqianyi.cs@gmail.com>
2018-06-18 16:35:24 +0900

    update docker image version to 1.0.5

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,7 @@
 docker run -ti --detach --name full-node \
        -v `pwd`:/etc/nodeos -v /tmp/nodeos-data:/data \
        -p 8888:8888 -p 9876:9876 \
-       johnnyzhao/eos:1.0.2.2 \
+       lemonswang/eos:mainnet-1.0.5 \
        /opt/eosio/bin/nodeos --data-dir=/data \
                              --config-dir=/etc/nodeos \
                              --genesis-json=/etc/nodeos/genesis.json \
